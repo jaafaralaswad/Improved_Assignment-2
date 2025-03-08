f8280415f64d1f6bcb82e72762007877e5ebf139
Jaafar Alaswad <jalaswad@bu.edu>
2025-03-07 23:07:45 -0500

    Update tests

---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import math
+import pytest
+
+# Import functions
+from direct_stiffness_method.direct_stiffness_method import Structure, StiffnessMatrices, BoundaryConditions, Solver, BucklingAnalysis, PlotResults
+
+# Testing Class Structure for rectangular and circular cross-sections
